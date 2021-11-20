6bf4b6b9db0c59607ef018f3cd0b57e8a407fe34
user <user@users-MBP.broadband>
2021-11-20 17:24:21 +0000

    yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from "yup";
+
+const SigninSchema = Yup.object().shape({
+email: Yup.string()
+.required("Email address is required")
+.email("This is not a valid email address"),
+password: Yup.string().required("Password is required").min(8, "Password must be atleast 8 characters")
+});
 
 function App() {
   const handleSubmit = (values: any) => {
@@ -11,11 +19,13 @@ function App() {
     <Formik
     initialValues = {{ email: '', password: ""}}
     onSubmit={handleSubmit}
+    validationSchema={SigninSchema}
     >
       {(props: any) => ( <Form> 
         <div>
           <label htmlFor="email">Email: </label>
           <Field name="email" />
+          <ErrorMessage name="email"/>
           </div>
           <div>
           <label htmlFor="password">Password: </label>

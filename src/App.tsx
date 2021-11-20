d9cefbc9fb71b3587feec3c1b584efea3c90b740
user <user@users-MBP.broadband>
2021-11-20 16:56:27 +0000

    formik field

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Field, Form, Formik } from 'formik';
 
 function App() {
+  const handleSubmit = (values: any) => {
+
+  }
   return (
     <div className="App">
-     <h1> hello </h1>
+    <Formik
+    initialValues = {{ email: '', password: ""}}
+    onSubmit={handleSubmit}
+    >
+      {(props: any) => ( <Form> 
+        <div>
+          <label htmlFor="email">Email: </label>
+          <Field name="email" />
+          </div>
+      </Form>
+        )}
+
+    </Formik>
     </div>
   );
 }
